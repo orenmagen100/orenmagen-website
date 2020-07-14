a5d00b11596d484837847355be12a2337e0f1091
Oren Magene <orenmagen100@gmail.com>
2020-07-14 19:59:40 +0300

    adding stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const divStyle = {
+  fontSize: '2.5rem',
+  fontStyle: 'italic',
+  fontWeight: 'bold',
+  paddingBottom: '10px'
+};
+
 function App() {
+  const [isHover, setIsHover] = useState(false);
+
+  const styles = {
+    backgroundColor: !isHover ? '#dca160' : '#eac69f',
+    color: !isHover ? '#28345e' : 'rgb(-64, 12, 54)',
+    cursor: 'pointer'
+  };
+  const imgStyle = {
+    cursor: 'pointer'
+  };
+  function toggleHover() {
+    setIsHover(!isHover);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <header className='App-header'>
+        <p style={divStyle}> Journey To Handstand - Oren Magen </p>
+        <img
+          style={imgStyle}
+          src='../profilePic.png'
+          alt='logo'
+          onClick={e => {
+            e.preventDefault();
+            window.location.href =
+              'https://www.udemy.com/course/journey-to-handstand/';
+          }}
+        />
+        <p>Now On Udemy! </p>
+        <button
+          className='App-link'
+          style={styles}
+          onMouseOver={toggleHover}
+          onMouseOut={toggleHover}
+          onClick={e => {
+            e.preventDefault();
+            window.location.href =
+              'https://www.udemy.com/course/journey-to-handstand/';
+          }}
         >
-          Learn React
-        </a>
+          Take me to the course
+        </button>
       </header>
     </div>
   );
